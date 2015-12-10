46122d6d16e9db5db73c9234604920858522378d
Tina Dominguez <tina@tinadominguez.com>
2015-12-09 20:47:29 -0500

    JQuery Plugin: Social Sharing
    Third plugin installed, removed footer to avoid clutter

--- js/main.js
+++ js/main.js
@@ -1,14 +1,20 @@
 /* This is the JQuery Plugin for the menu */
 
-$(function() {
+$document.ready(function() {
   $('#main-menu').smartmenus();
 });
 
-/* This is for the Twitter Plugin on About page */
+/* This is for the social sharing plugin */
 
-$(document).ready(function() {
-   ("#tweets").tweetSlider({
-        hash:"pizzaplanet",
-       refreshTime: 50000,
-    });
-    })
+$(document).ready(function ($) {
+
+  $('.rrssb-buttons').rrssb({
+    // required:
+    title: 'mmmm pizza',
+    url: 'http://tinadominguez.com/project_final3_dominguez_cristina/'
+
+    // optional:
+    description: 'This amazing site is all about pizza',
+    emailBody: 'My favorite kind of pizza is Hawaiian, because pineapples are necessary.'
+  });
+});
